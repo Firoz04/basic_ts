19fa5708793113ab838100a15738841e30a0ea12
Firoz04 <firoz15-4313@diu.edu.bd>
2025-12-04 19:05:22 +0600

    add new files

---
+++ user-defind-type/tuples-type.js
@@ -0,0 +1,7 @@
+//mixed data : key,value
+var user;
+user = ["Firoz", 199];
+//or, let user: [string, number] = ["Firoz", 199];
+user.push("Shakib", 200);
+console.log(user);
+console.log(user[1]);
---
+++ user-defind-type/tuples-type.ts
@@ -0,0 +1,9 @@
+//mixed data : key,value
+let user : [string, number];
+user = ["Firoz", 199]
+//or, let user: [string, number] = ["Firoz", 199];
+
+user.push("Shakib",200);
+
+console.log(user);
+console.log(user[1]);
