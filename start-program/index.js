116c8a83fb36fb12f037cd48c29405bcbf4ee84b
Firoz04 <firoz15-4313@diu.edu.bd>
2025-12-10 23:32:44 +0600

    add new files

--- start-program/index.js
+++ start-program/index.js
@@ -3,3 +3,13 @@ function greet(name) {
 }
 var message = greet("World");
 console.log(message);
+// String type examples
+var color = "blue";
+var fullName = 'John Doe';
+var age = 30;
+var sentence = "Hello, my name is ".concat(fullName, " and I'll be ").concat(age + 1, " next year.");
+// Output the values
+console.log(color);
+console.log(fullName);
+console.log(age);
+console.log(sentence);
--- start-program/index.ts
+++ start-program/index.ts
@@ -5,3 +5,14 @@ function greet(name: string): string {
 const message: string = greet("World");
 console.log(message);
 
+// String type examples
+let color: string = "blue";
+let fullName: string = 'John Doe';
+let age: number = 30;
+let sentence: string = `Hello, my name is ${fullName} and I'll be ${age + 1} next year.`;
+
+// Output the values
+console.log(color);
+console.log(fullName);
+console.log(age);
+console.log(sentence);
